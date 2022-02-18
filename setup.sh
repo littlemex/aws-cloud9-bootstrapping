f8d95e35bc1421a42e278824906a845229a03d05
EC2 Default User <ec2-user@ip-172-31-5-199.ec2.internal>
2022-02-18 09:56:11 +0000

    cloud9-cfn

---
+++ setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Simple bash for Demoset C9 Deployment. 
+# NOT FOR PRODUCTION USE - Only for Demoset purposes
+C9STACK="DemosetC9"
+PROFILE=$2
+REGION=$1 
+
+if ! [ -x "$(command -v aws)" ]; then
+  echo 'Error: aws cli is not installed.' >&2
+  exit 1
+fi
+
+if [ ! -z  "$PROFILE" ]; then
+  export AWS_PROFILE=$PROFILE
+fi
+
+if [ ! -z "$REGION" ]; then
+  export AWS_DEFAULT_REGION=$REGION
+fi
+
+echo Building $PROFILE C9
+
+# build the c9 environment
+aws cloudformation deploy  --stack-name $C9STACK --capabilities CAPABILITY_IAM --template ./example_instancestack.yaml
+
+exit 0 
